7639f9fc2629f5f5115dc7761070e9dc13e42141
KevinAngelP <kevinangelperez71@alumnes.ilerna.com>
2024-12-23 15:36:45 +0100

    Update lagarto.java

--- iguana/src/iguana/lagarto.java
+++ iguana/src/iguana/lagarto.java
@@ -1,5 +1,27 @@
-package iguana;
+import java.util.Random;
 
-public class lagarto {
+public class iguana {
 
+    public static void main(String[] args) {
+        int filas = 5;  // Puedes cambiar el número de filas
+        int columnas = 5;  // Puedes cambiar el número de columnas
+        
+        int[][] matriz = new int[filas][columnas];
+
+        Random random = new Random();
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j] = random.nextInt(100); 
+            }
+        }
+
+        System.out.println("Matriz de números aleatorios:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + "\t"); // Imprimir el número con tabulación
+            }
+            System.out.println();
+        }
+    }
 }
